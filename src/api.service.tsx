fd32e16a23ec727884fd75de0a160279e1924fe0
Michel dos Santos Kuguio <michel.kuguio@gmail.com>
2026-04-04 16:54:17 -0300

    fix: env

--- src/api.service.tsx
+++ src/api.service.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {toast} from "react-toastify";
 
-const api = axios.create({baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000'})
+const api = axios.create({baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'})
 
 
 api.interceptors.response.use((success) => {
